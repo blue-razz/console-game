eb59d7501be149a5ad2f467c17f6f29a5ebc14d5
blue-razz <91354683+blue-razz@users.noreply.github.com>
2022-04-13 17:22:35 -0700

    added some mountain and tree generation

--- src/game/world.rs
+++ src/game/world.rs
@@ -1,4 +1,7 @@
 use super::player::Player;
+use colored::*;
+use rand::distributions::Uniform;
+use rand::prelude::*;
 use std::io;
 use std::io::Write;
 
@@ -9,7 +12,7 @@ pub struct World {
     pub height: i32,
     pub width: i32,
     pub map: Vec<Vec<char>>,
-    player_list: Vec<Player>
+    player_list: Vec<Player>,
 }
 
 impl World {
@@ -19,40 +22,77 @@ impl World {
             height,
             width,
             player_list: vec![],
-            map: vec![vec!['.'; width as usize]; height as usize]
+            map: vec![vec!['.'; width as usize]; height as usize],
         }
     }
 
     pub fn spawn_player(&mut self, player_name: String) {
-        self.player_list.push(Player::new(player_name, vec![16, 40]));
+        self.player_list
+            .push(Player::new(player_name, vec![16, 40]));
     }
 
     // Generate the world given the world's height and width
     pub fn generate_world(&mut self) {
         let MAX_Y: i32 = self.height - 1;
         let MAX_X: i32 = self.width - 1;
-        
+
         // Generate axis
         for y in 0..self.height {
-            for x in 0..self.width { 
+            for x in 0..self.width {
                 // Is this the first row?
                 if y == 0 {
                     self.map[y as usize][x as usize] = 'M';
                 }
-
                 // Is this the last row?
                 else if y == MAX_Y {
                     self.map[y as usize][x as usize] = 'M';
                 }
-
                 // Is this the first column?
                 else if x == 0 {
                     self.map[y as usize][x as usize] = 'M';
                 }
-
                 // Is this the last column?
                 else if x == MAX_X {
                     self.map[y as usize][x as usize] = 'M';
+                } else {
+                    // Generate a random numbeer between 0 and 1
+                    let mut rng = rand::thread_rng();
+                    let rand_num = Uniform::new(0, 100).sample(&mut rng);
+
+                    // Generate more mountains as we get closer to the borders
+                    if y < 6 || y > MAX_Y - 6 {
+                        if rand_num > 50 {
+                            self.map[y as usize][x as usize] = 'M';
+                        }
+                    } else if x < 6 || x > MAX_X - 6 {
+                        if rand_num > 50 {
+                            self.map[y as usize][x as usize] = 'M';
+                        }
+                    } else if y > 5 && y < 8 || y < MAX_Y - 8 && y > MAX_Y - 5 {
+                        if rand_num > 90 {
+                            self.map[y as usize][x as usize] = 'M';
+                        }
+                    } else if x > 5 && x < 12 || x > MAX_X - 12 && x < MAX_X - 5 {
+                        if rand_num > 95 {
+                            self.map[y as usize][x as usize] = 'M';
+                        }
+                    }
+
+                    // Sprinkle some monutains into the mix
+                    else {
+                        if rand_num == 47 {
+                            self.map[y as usize][x as usize] = 'M';
+                        }
+                    }
+                    
+                    // TODO: Generate rivers
+                    
+                    // Geneerate some trees
+                    if y > 6 && y < MAX_Y - 6 && x > 6 && x < MAX_X - 6 {
+                        if rand_num > 90 && self.map[y as usize][x as usize] != 'M' {
+                            self.map[y as usize][x as usize] = 'T';
+                        }
+                    }
                 }
             }
         }
@@ -60,8 +100,16 @@ impl World {
 
     fn tile_has_player(&self, pos: Vec<i32>) -> bool {
         for p in &self.player_list {
-            let player = p.player_pos.iter().zip(pos.iter()).filter(|(u, t)| u == t).count() == 2;
-            if player { return true; }
+            let player = p
+                .player_pos
+                .iter()
+                .zip(pos.iter())
+                .filter(|(u, t)| u == t)
+                .count()
+                == 2;
+            if player {
+                return true;
+            }
         }
 
         false
@@ -71,11 +119,17 @@ impl World {
         for y in 0..self.height {
             for x in 0..self.width {
                 if self.tile_has_player(vec![y, x]) {
-                    print!("O");
+                    print!("{}", "@".magenta().blink());
                 } else {
-                    print!("{}", &self.map[y as usize][x as usize]);
+                    if self.map[y as usize][x as usize] == 'M' {
+                        print!("{}", "M".truecolor(255, 248, 220));
+                    } else if self.map[y as usize][x as usize] == 'T' {
+                        print!("{}", "T".bright_green().bold());
+                    } else {
+                        print!("{}", ".".green());
+                    }
                 }
-                
+
                 io::stdout().flush().unwrap();
             }
 
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ mod game;
 use game::world::World;
 
 fn main() {
-    let mut world = World::new(String::from("overworld"), 32, 64);
+    let mut world = World::new(String::from("overworld"), 52, 150);
     world.generate_world();
     world.spawn_player(String::from("bob"));
     world.print();
